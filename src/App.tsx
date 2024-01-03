d6ebe865d2cddd3b67748c1820034eca5628e579
KDBismael <ismaelkondombo1@gmail.com>
2024-01-03 09:45:29 -0800

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex items-center justify-center'>
+        <h1>Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
